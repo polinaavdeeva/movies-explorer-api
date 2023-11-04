6357f82c5ae867e4f2b556efee406f1519dd3d61
Polina Avdeeva <polinaavdeeva55@gmail.com>
2023-11-04 22:35:12 +0300

    refactor: rename movie item

--- models/movie.js
+++ models/movie.js
@@ -30,7 +30,7 @@ const movieSchema = new mongoose.Schema({
       message: 'Некорректная ссылка',
     },
   },
-  trailer: {
+  trailerLink: {
     type: String,
     required: true,
     validate: {
